5acb9d31407e2721039333c3ccb0d8f8bca381c8
Matthew Schurr <mschurr@rice.edu>
2014-02-15 03:05:56 -0600

    Cleaned up a bit.

---
+++ webapp/appengine_config.tmpl.py
@@ -0,0 +1,11 @@
+"""`appengine_config` gets loaded when starting a new application instance."""
+import sys
+import os.path
+# add `lib` subdirectory to `sys.path`, so our `main` module can load
+# third-party libraries.
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
+
+from gaesessions import SessionMiddleware
+def webapp_add_wsgi_middleware(app):
+	app = SessionMiddleware(app, cookie_key="KEY")
+ 	return app
