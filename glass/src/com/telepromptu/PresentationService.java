15892393b2c2793e39992a5d72c878263da65e8d
Salvatore Testa <sal.testa@gmail.com>
2014-02-15 16:06:19 -0500

    develop audio processing in a greater way

--- glass/src/com/telepromptu/PresentationService.java
+++ glass/src/com/telepromptu/PresentationService.java
@@ -1,6 +1,8 @@
 package com.telepromptu;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import com.google.android.glass.timeline.LiveCard;
 import com.google.android.glass.timeline.TimelineManager;
@@ -17,7 +19,6 @@ import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.util.Log;
 import android.widget.RemoteViews;
-import android.widget.TextView;
 
 public class PresentationService extends Service {
 
@@ -25,10 +26,8 @@ public class PresentationService extends Service {
     private static final String LIVE_CARD_TAG = "presentation";
     private LiveCard mLiveCard;
     private TimelineManager mTimelineManager;
-    private TextView cardText;
     private RemoteViews remoteViews;
     private SpeechRecognizer speechRecognizer;
-    private Handler mainHandler;
     
     @Override
     public void onCreate() {
@@ -36,7 +35,6 @@ public class PresentationService extends Service {
         speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);       
         speechRecognizer.setRecognitionListener(new DictationListener()); 
         mTimelineManager = TimelineManager.from(this);
-        mainHandler = new Handler(this.getMainLooper());
     }
     
 	@Override
@@ -49,20 +47,8 @@ public class PresentationService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         if (mLiveCard == null) {
             Log.d(TAG, "Publishing LiveCard");
-            mLiveCard = mTimelineManager.createLiveCard(LIVE_CARD_TAG);
-            remoteViews = new RemoteViews(this.getPackageName(), R.layout.card_speech_recognition);
-            mLiveCard.setViews(remoteViews);
-            Intent menuIntent = new Intent(this, MenuActivity.class);
-            menuIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            mLiveCard.setAction(PendingIntent.getActivity(this, 0, menuIntent, 0));
-	        
-            Intent speechIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);        
-            speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-            speechIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,this.getPackageName());
-            speechIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS,1);
-            speechRecognizer.startListening(speechIntent);
-            
-            mLiveCard.publish(PublishMode.REVEAL);
+            makeCard("Yolo");
+            startListening();
             Log.d(TAG, "Done publishing LiveCard");
         } else {
             // TODO(alainv): Jump to the LiveCard when API is available.
@@ -71,28 +57,50 @@ public class PresentationService extends Service {
         return START_STICKY;
     }
 
+    private void makeCard(String message) {
+    	mLiveCard = mTimelineManager.createLiveCard(LIVE_CARD_TAG);
+        remoteViews = new RemoteViews(this.getPackageName(), R.layout.card_speech_recognition);
+        remoteViews.setCharSequence(R.id.current_text, "setText", message);
+        mLiveCard.setViews(remoteViews);
+        Intent menuIntent = new Intent(this, MenuActivity.class);
+        menuIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        mLiveCard.setAction(PendingIntent.getActivity(this, 0, menuIntent, 0));
+        mLiveCard.publish(PublishMode.REVEAL);
+    }
+    
+    private void destroyCard() {
+    	 if (mLiveCard != null && mLiveCard.isPublished()) {
+             Log.d(TAG, "Unpublishing LiveCard");
+             mLiveCard.unpublish();
+             mLiveCard = null;
+         }
+    }
+    
     @Override
     public void onDestroy() {
-        if (mLiveCard != null && mLiveCard.isPublished()) {
-            Log.d(TAG, "Unpublishing LiveCard");
-            mLiveCard.unpublish();
-            mLiveCard = null;
-        }
+    	destroyCard();
+        speechRecognizer.destroy();
         super.onDestroy();
     }
     
-    private void updateText(String text) {
-        if (mLiveCard != null && mLiveCard.isPublished()) {
-        	remoteViews.setCharSequence(R.id.current_text, "setText", text);
-        	Log.d(TAG, "Updated the slide text");
-        }
+    private void startListening() {
+    	speechRecognizer.stopListening();
+        Intent speechIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);        
+        speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        speechIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,this.getPackageName());
+        speechIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS,1);
+        speechIntent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS,true);
+        speechRecognizer.startListening(speechIntent);
+    }
+    
+    private void exit() {
+    	this.stopSelf();
     }
     
     private class DictationListener implements RecognitionListener {
 
 		@Override
 		public void onBeginningOfSpeech() {
-			updateText("Starting speech");
         	Log.d(TAG, "Starting speech");
 		}
 
@@ -102,6 +110,7 @@ public class PresentationService extends Service {
 
 		@Override
 		public void onEndOfSpeech() {
+        	Log.d(TAG, "onEndOfSpeech");
 		}
 
 		@Override
@@ -109,11 +118,22 @@ public class PresentationService extends Service {
 		}
 
 		@Override
-		public void onEvent(int arg0, Bundle arg1) {
+		public void onEvent(int arg0, Bundle results) {
+			Log.d(TAG, "onEvent of speech");
+			ArrayList<String> data = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+			if(data.size() > 0) {
+	        	Log.d(TAG, data.get(0));
+			}
 		}
 
 		@Override
-		public void onPartialResults(Bundle arg0) {
+		public void onPartialResults(Bundle results) {
+			Log.d(TAG, "onPartialResults of speech");
+			ArrayList<String> data = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+			if(data.size() > 0) {
+	        	Log.d(TAG, data.get(0));
+	        	makeCard(data.get(0));
+			}
 		}
 
 		@Override
@@ -125,9 +145,16 @@ public class PresentationService extends Service {
         	Log.d(TAG, "onResult of speech");
 			ArrayList<String> data = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
 			if(data.size() > 0) {
-				updateText(data.get(0));
 	        	Log.d(TAG, data.get(0));
+	        	makeCard(data.get(0));
 			}
+			(new Timer()).schedule(new TimerTask() {
+				
+				@Override
+				public void run() {
+					exit();					
+				}
+			}, 2000);
 		}
 
 		@Override
