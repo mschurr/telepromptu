102b99ba986f9ae6896ace6105e2c127227d71a9
Matthew Schurr <mschurr@rice.edu>
2014-02-16 07:02:56 -0600

    Web app changes.

--- webapp/models.py
+++ webapp/models.py
@@ -3,3 +3,7 @@ from oauth2client.appengine import CredentialsProperty
 
 class UserCredentials(db.Model):
 	credentials = CredentialsProperty()
+
+class PresentationLink(db.Model):
+	driveid = db.StringProperty()
+	userid = db.StringProperty()
